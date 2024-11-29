9ff819b884d8cc1c54cfb9754ee5197dbe14edd0
deepakacharyak <deepakacharyak887@gmail.com>
2024-11-29 07:49:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.to(".imagecontainer",{
+    width:'100%',
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2
+})
+
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    top:'0%',
+    duration:2,
+    stagger:2
+})
+
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    top:'-100%',
+    delay:2,
+    color:'transparent',
+    duration:2,
+    stagger:2
+})
